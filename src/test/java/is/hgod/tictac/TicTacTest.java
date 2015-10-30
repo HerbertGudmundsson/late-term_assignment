cc2aebaaa17f58aedf24e2a78de9f50dfec5f050
Árni Oddsson <arniodds@gmail.com>
2015-10-30 13:28:22 +0000

    Testing gradle

---
+++ src/test/java/is/hgod/tictac/TicTacTest.java
@@ -0,0 +1,26 @@
+package com.hgod.TicTac;
+
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class TicTacTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("com.hgod.late-term_assignment.TicTacTest");
+    }
+
+    @Test
+    public void return3(){
+    	assertEquals(3, TicTac.return3());
+    }
+
+    @Test
+    public void return2(){
+    	assertEquals(2, TicTac.return2());
+    }
+
+    @Test
+    public void return1(){
+    	assertEquals(1, TicTac.return1());
+    }
+}
