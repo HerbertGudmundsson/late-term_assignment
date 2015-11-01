54e01050c04d9cdbeca2e02ef23b93bc88339720
Árni Oddsson <arniodds@gmail.com>
2015-11-01 18:19:51 +0000

    Fixing merge issues

--- src/main/java/is/hgod/tictac/TicTac.java
+++ src/main/java/is/hgod/tictac/TicTac.java
@@ -1,24 +1,6 @@
 package is.hgod.tictac;
 
 public class TicTac{
-
-        public static int return3(){
-		return 3;
-	}
-		public static int return2(){
-		return 2;
-	}
-		public static int return1(){
-		return 1;
-	}
-
-	public static void test(){
-		boolean thuglife = true;
-		if(thuglife){
-
-		}
-	}
     public static void main(String[] args){
-	System.out.println(1);
     }
 }
--- src/test/java/is/hgod/tictac/TicTacTest.java
+++ src/test/java/is/hgod/tictac/TicTacTest.java
@@ -8,19 +8,4 @@ public class TicTacTest {
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.hgod.late-term_assignment.TicTacTest");
     }
-
-    @Test
-    public void return3(){
-    	assertEquals(3, TicTac.return3());
-    }
-
-    @Test
-    public void return2(){
-    	assertEquals(2, TicTac.return2());
-    }
-
-    @Test
-    public void return1(){
-    	assertEquals(1, TicTac.return1());
-    }
 }
